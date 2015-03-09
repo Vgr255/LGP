5f1a327f24376e46fd48d431aa0444ff9535882e
Vgr E.Barry <vgr255@live.ca>
2015-03-09 14:06:04 -0400

    Scrap threading for parsing the ToC.

--- lgp.py
+++ lgp.py
@@ -26,9 +26,7 @@ __author__ = "Vgr"
 
 _libname_ = __file__[-list(reversed(__file__.replace("\\", "/"))).index("/"):]
 
-import threading
 import hashlib
-import time
 import sys
 import os
 
@@ -40,24 +38,6 @@ _hashed_files = {}
 # after this, we're saving the files' contents themselves in memory
 # this is all optimization, and is only used to access the data more than once
 _files_contents = {}
-
-# this boolean determines if threading is to be used when parsing the contents
-# it can be re-assigned after the module has been imported
-USE_THREADING = True
-
-# this is the maximum number of ToC entries that will be parsed per thread,
-# if the above is set to True
-MAX_LOOKUP_VALUE_PER_THREAD = 200
-
-# the main thread will need to access this lock multiple times
-# every other thread needs to access it only once
-# however, we need a reentrant lock because of the main thread
-ToC_Lock = threading.RLock()
-
-# global mutable objects used for multi-threading
-_all_files = {}
-_has_conflicts = []
-_uniques = []
 
 def _join_hex(hexlist):
     allhexes = reversed([hex(int(str(x))) for x in hexlist])
@@ -73,33 +53,7 @@ def _join_bytes(bytelist):
         new += char
     return new
 
-def _handle_multi_threading(num, toc):
-    has_conflicts = False
-    threads_started = 0
-    pointer = 16
-    while num > 0:
-        n = MAX_LOOKUP_VALUE_PER_THREAD
-        if num < MAX_LOOKUP_VALUE_PER_THREAD:
-            n = num
-        _uniques.append(num)
-        threading.Thread(None, _parse_toc, args=(n, toc[:n*27], pointer, num)).start()
-        toc = toc[n:]
-        pointer += n*27
-        num -= MAX_LOOKUP_VALUE_PER_THREAD
-        threads_started += 1
-    while True:
-        with ToC_Lock:
-            if not _uniques:
-                return (_all_files, any(_has_conflicts))
-        # it takes roughly 7/200000 of a second per ToC entry
-        # rough that up to 10/200000 (or 1/20000) per entry
-        # that number, times the maximum amount of entry lookups per thread
-        # and that, times the number of threads started, will give us a
-        # rough estimate of when all the threads will have completed
-        # this will almost never exceed one second
-        time.sleep((1/20000 * MAX_LOOKUP_VALUE_PER_THREAD) * threads_started)
-
-def _parse_toc(num, toc, pointer=16, unique=None):
+def _parse_toc(num, toc, pointer=16):
     has_conflicts = False
     files = {}
     while num:
@@ -127,13 +81,7 @@ def _parse_toc(num, toc, pointer=16, unique=None):
             has_conflicts = True
         num -= 1
 
-    if USE_THREADING:
-        with ToC_Lock:
-            _all_files.update(files)
-            _has_conflicts.append(has_conflicts)
-            _uniques.remove(unique)
-    else:
-        return (files, has_conflicts)
+    return (files, has_conflicts)
 
 def read(file):
     with open(file, "rb") as f:
@@ -149,16 +97,12 @@ def read(file):
         _hashed_files[file] = fhash
         _files_contents[file] = [[], {}, _all]
         # the first 12 bytes are the file creator; right-aligned
-        fcreator, _all = (_all[:12], _all[12:])
-        # next up is the amount of files contained in the archive (4 bytes)
-        num, _all = (_all[:4], _all[4:])
+        # then the number of files in the archive (4 bytes integer)
+        fcreator, num, _all = (_all[:12], _all[12:16], _all[16:])
         # find the actual value of the byte we just got
         # flip the bytes around and calculate that
         num = int(_join_hex(num), 16)
-        if USE_THREADING:
-            files, has_conflicts = _handle_multi_threading(num, _all[:num*27])
-        else:
-            files, has_conflicts = _parse_toc(num, _all[:num*27])
+        files, has_conflicts = _parse_toc(num, _all[:num*27])
         _all = _all[num*27:]
         # past this point, we parsed and saved all files' offsets
         # let's sort the files by order that they appear to find the first
